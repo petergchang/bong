d30affa78c474fc04e9e5c858cd92025a2dc1d62
Peter G. Chang <peter0414@gmail.com>
2024-05-12 12:20:51 -0500

    Add hyperparameter folders

--- bong/settings.py
+++ bong/settings.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
+# Hyperparameters
+hparam_path = Path(Path(__file__).resolve().parent, "hyperparameters")
+hparam_mnist_path = Path(hparam_path, "mnist_hyperparameters")
+
 # Result
 result_path = Path(Path(__file__).resolve().parent, "results")
 linreg_path = Path(result_path, "linreg_results")
 logreg_path = Path(result_path, "logreg_results")
 uci_path = Path(result_path, "uci_results")
+mnist_path = Path(result_path, "mnist_results")
