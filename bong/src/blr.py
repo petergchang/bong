7fc5f13e11a414fa8d27bc1efe127803764d8741
Peter G. Chang <peter0414@gmail.com>
2024-10-25 18:59:37 -0400

    Implement BLR-FC-LIN-EF

--- bong/src/blr.py
+++ bong/src/blr.py
@@ -193,22 +193,42 @@ def update_lfg_blr(
     """
     mean0, cov0 = state_pred
     mean, cov = state
-    y_pred = jnp.atleast_1d(emission_mean_function(mean, x))
-    H = jnp.atleast_2d(jax.jacrev(emission_mean_function)(mean, x))
     R = jnp.atleast_2d(emission_cov_function(mean, x))
-    R_inv = jnp.linalg.pinv(R)
-    prec0, prec = jnp.linalg.pinv(cov0), jnp.linalg.pinv(cov)
-    new_prec = (
-        (1 - learning_rate) * prec
-        + learning_rate * prec0
-        + learning_rate * H.T @ R_inv @ H
-    )
-    new_cov = jnp.linalg.pinv(new_prec)
-    new_mean = (
-        mean
-        + learning_rate * new_cov @ prec0 @ (mean0 - mean)
-        + learning_rate * new_cov @ H.T @ R_inv @ (y - y_pred)
-    )
+    if empirical_fisher:
+        R_inv = jnp.linalg.lstsq(R, jnp.eye(R.shape[0]))[0]
+
+        def ll_fn(params):
+            y_pred = emission_mean_function(params, x)
+            return -0.5 * (y - y_pred).T @ R_inv @ (y - y_pred)
+
+        grad = jax.grad(ll_fn)(mean)
+        G = -jnp.outer(grad, grad)
+        prec0, prec = jnp.linalg.pinv(cov0), jnp.linalg.pinv(cov)
+        new_prec = (
+            (1 - learning_rate) * prec + learning_rate * prec0 - learning_rate * G
+        )
+        new_cov = jnp.linalg.pinv(new_prec)
+        new_mean = (
+            mean
+            + learning_rate * new_cov @ prec0 @ (mean0 - mean)
+            + learning_rate * new_cov @ grad
+        )
+    else:
+        y_pred = jnp.atleast_1d(emission_mean_function(mean, x))
+        H = jnp.atleast_2d(jax.jacrev(emission_mean_function)(mean, x))
+        R_inv = jnp.linalg.pinv(R)
+        prec0, prec = jnp.linalg.pinv(cov0), jnp.linalg.pinv(cov)
+        new_prec = (
+            (1 - learning_rate) * prec
+            + learning_rate * prec0
+            + learning_rate * H.T @ R_inv @ H
+        )
+        new_cov = jnp.linalg.pinv(new_prec)
+        new_mean = (
+            mean
+            + learning_rate * new_cov @ prec0 @ (mean0 - mean)
+            + learning_rate * new_cov @ H.T @ R_inv @ (y - y_pred)
+        )
     new_state = AgentState(new_mean, new_cov)
     return new_state
 
